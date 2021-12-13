ae31c2f0db3a876290ecfe506d1bdd5de6129419
kongom2 <kongom2@jsceno.com>
2021-12-14 03:31:42 +0900

    로그인, 회원가입 화면 추가

--- clone/src/elements/Image.js
+++ clone/src/elements/Image.js
@@ -1,43 +1,66 @@
-import React from 'react'
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
 const Image = (props) => {
-    const {src, size, bgsize} = props
+  const {
+    imageType,
+    imageUrl,
+    size,
+    backgroundSize,
+    width,
+    height,
+    margin,
+    padding,
+  } = props;
 
-    const styles = { // style끼리 구분하는게 편해서
-        src : src,
-        size: size,
-        bgsize: bgsize
-    }
-
-        return(
-            <>
-            <OutBox>
-              <InBox {...styles}/>  
-            </OutBox>
-            </>
-
-        )
-    }
+  const styles = {
+    // style끼리 구분하는게 편해서
+    size: size,
+    width: width,
+    height: height,
+    margin: margin,
+    padding: padding,
+    backgroundSize: backgroundSize,
+    imageUrl: imageUrl,
+  };
+  if (imageType === "logo") {
+    return (
+      <>
+        <ImageLogo {...styles}></ImageLogo>
+      </>
+    );
+  }
+};
 
 Image.defaultProps = {
-    shape : "rectangle",
-    src: "https://newsimg.hankookilbo.com/cms/articlerelease/2021/06/05/ef519975-80c8-40b6-b25a-47ab6270dc60.png",
-    size : 40,
-    bgsize : "cover",
-}
+  imageType: "logo", // 동그란 이미지, 사각형 포스트 이미지
+  src: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2560px-Instagram_logo.svg.png",
+  size: 40,
+  backgroundSize: "cover",
+  width: null,
+  height: null,
+  margin: null,
+  padding: null,
+};
 
-const OutBox = styled.div`
-    width: 100%;
-`
-const InBox= styled.div`
-position: relative; // 상대적인 포지션
-padding-top: 75%; // 75%를 주는 이유는 넓이가 100%이기 때문에 4:3비율을 맟추기 위해
-overflow: hidden; // 이 박스 영역을 벗어나면 숨겨버린다.
-background-image : url("${(props) => props.src}");
-background-size: ${(props)=> props.bgsize} ;
-`
+const ImageLogo = styled.div`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  margin: ${(props) => props.margin};
+  padding: ${(props) => props.padding};
+  background-size: ${(props) => props.backgroundSize};
+  background-image: url("${(props) => props.imageUrl}");
+`;
 
+// const OutBox = styled.div`
+//   width: 100%;
+// `;
+// const InBox = styled.div`
+//   position: relative; // 상대적인 포지션
+//   padding-top: 75%; // 75%를 주는 이유는 넓이가 100%이기 때문에 4:3비율을 맟추기 위해
+//   overflow: hidden; // 이 박스 영역을 벗어나면 숨겨버린다.
+//   background-image: url("${(props) => props.src}");
+//   background-size: ${(props) => props.bgsize};
+// `;
 
-
-export default Image
+export default Image;
--- clone/src/elements/Input.js
+++ clone/src/elements/Input.js
@@ -1,58 +1,87 @@
 import React from "react";
-import styled   from "styled-components";
+import styled from "styled-components";
 import Grid from "./Grid";
 
 const Input = (props) => {
-    const {type, value, placeholder, _onChange, width, multiLine, onSubmit, border} =props;   //InputSome 태그안에 {...props}라고 해도되는데 하다보면 props 에 InputSome에 들어가지않고 다른곳에서 쓰일수도있다. 따라서 필요없는걸 넣어줄필요없는데 다들어가게됨으로 그걸방하기위해 props안에있는걸 필요한거만 InputSome에 넣어줄것
-    
-    if(multiLine){
-        return (
-          <Grid>
-            <ElTextarea rows={5} value={value} placeholder={placeholder} onChange={_onChange}></ElTextarea>    {/* rows는 몇줄을넣을지 */}
-          </Grid>
-        )
-      }
-    
-    return(
-        <React.Fragment>
-            <Grid>
-            <InputSome border ={border} type={type} value={value} placeholder={placeholder} onChange={_onChange} width={width} 
-            onKeyPress={(e)=> {
-              if(e.key ==="Enter"){
-                onSubmit(e);
-              }
-            }}
-            ></InputSome> 
-            </Grid>
-        </React.Fragment>
-    )
-} 
+  const {
+    type,
+    value,
+    placeholder,
+    _onChange,
+    width,
+    // multiLine,
+    onSubmit,
+    border,
+    padding,
+    margin,
+  } = props; //InputSome 태그안에 {...props}라고 해도되는데 하다보면 props 에 InputSome에 들어가지않고 다른곳에서 쓰일수도있다. 따라서 필요없는걸 넣어줄필요없는데 다들어가게됨으로 그걸방하기위해 props안에있는걸 필요한거만 InputSome에 넣어줄것
+  const styles = {
+    padding: padding,
+    margin: margin,
+    placeholder: placeholder,
+    width: width,
+    border: border,
+  };
+  // if (multiLine) {
+  //   return (
+  //     <Grid>
+  //       <ElTextarea
+  //         {...styles}
+  //         rows={5}
+  //         value={value}
+  //         onChange={_onChange}
+  //       ></ElTextarea>{" "}
+  //       {/* rows는 몇줄을넣을지 */}
+  //     </Grid>
+  //   );
+  // }
+
+  return (
+    <React.Fragment>
+      <Grid>
+        <InputSome
+          {...styles}
+          type={type}
+          value={value}
+          onChange={_onChange}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              onSubmit(e);
+            }
+          }}
+        ></InputSome>
+      </Grid>
+    </React.Fragment>
+  );
+};
 
 Input.defaultProps = {
-    type: "text",
-    value: undefined,
-    placeholder: "입력해주세요.",
-    label : null, 
-    _onChange : () => {},
-    width : null,
-    onSubmit : () =>{},
-    border: "",
-    
-}
+  type: "text",
+  value: undefined,
+  placeholder: "입력해주세요.",
+  label: null,
+  _onChange: () => {},
+  onSubmit: () => {},
+  border: "",
+  padding: null,
+  margin: null,
+  width: "100%",
+};
 
-const ElTextarea = styled.textarea`
-    border: 1px solid #212121;
-    width: 100%;
-    padding: 12px 4px;
-    box-sizing: border-box;
-`;
+// const ElTextarea = styled.textarea`
+//   border: 1px solid #212121;
+//   width: 100%;
+//   padding: 12px 4px;
+//   box-sizing: border-box;
+// `;
 
 const InputSome = styled.input`
-  border: 1px solid #212121;
+  border: 1px solid #e4e4e4;
   box-sizing: border-box;
-  width: ${(props)=>props.width};
-  border : ${(props)=> props.border};
+  width: 100%;
+  border: ${(props) => props.border};
+  padding: ${(props) => props.padding};
+  margin: ${(props) => props.margin};
 `;
-
 
 export default Input;
