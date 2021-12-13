e57a3010c2a9d5578f4cd832db944b9db16087fa
kongom2 <kongom2@jsceno.com>
2021-12-13 21:48:52 +0900

    iconButton 추가

--- clone/src/elements/IconButton.js
+++ clone/src/elements/IconButton.js
@@ -1,14 +1,24 @@
 import React from "react";
 import styled from "styled-components";
-import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import {
+  FcLike, // 좋아요 선택
+  AiOutlineHeart, // 좋아요 해제
+  FaRegComment, // 코멘트 아이콘
+  MdOutlineAddBox, // 포스트 추가 버튼
+  AiOutlineCheck, // 게시물 등록 버튼
+  BiArrowBack, // 뒤로가기 버튼
+} from "react-icons/ai";
 
 const ImageButton = (props) => {
   // props
   const {
-    deleteIcon, // 딜리트 아이콘
     _onClick,
-    _onChange,
-    onSubmit,
+    likeIcon, // 좋아요 아이콘
+    unLikeIcon, // 좋아요 해제 아이콘
+    commentIcon, // 코멘트 아이콘
+    plusIcon, // 포스트 추가 아이콘
+    checkIcon, // 게시물 등록 아이콘
+    leftArrowIcon, // 뒤로가기 아이콘
     size,
     height,
     margin,
@@ -16,45 +26,65 @@ const ImageButton = (props) => {
 
   // props style
   const styles = {
+    size: size,
     height: height,
     margin: margin,
   };
 
-  if (props.deleteIcon) {
+  if (likeIcon) {
     return (
       <React.Fragment>
         <Icon {...styles}>
-          <AiFillDelete
-            size={size}
-            onClick={_onClick}
-            onChange={_onChange}
-            onKeyPress={(e) => {
-              if (e.key === "Enter") {
-                onSubmit(e);
-              }
-            }}
-          ></AiFillDelete>
+          <FcLike size={size} onClick={_onClick}></FcLike>
         </Icon>
       </React.Fragment>
     );
   }
-
-  return (
-    <React.Fragment>
-      <Icon {...styles}>
-        <AiFillEdit
-          size={size}
-          onClick={_onClick}
-          onChange={_onChange}
-          onKeyPress={(e) => {
-            if (e.key === "Enter") {
-              onSubmit(e);
-            }
-          }}
-        ></AiFillEdit>
-      </Icon>
-    </React.Fragment>
-  );
+  if (unLikeIcon) {
+    return (
+      <React.Fragment>
+        <Icon {...styles}>
+          <AiOutlineHeart size={size} onClick={_onClick}></AiOutlineHeart>
+        </Icon>
+      </React.Fragment>
+    );
+  }
+  if (commentIcon) {
+    return (
+      <React.Fragment>
+        <Icon {...styles}>
+          <FaRegComment size={size} onClick={_onClick}></FaRegComment>
+        </Icon>
+      </React.Fragment>
+    );
+  }
+  if (plusIcon) {
+    return (
+      <React.Fragment>
+        <Icon {...styles}>
+          <MdOutlineAddBox size={size} onClick={_onClick}></MdOutlineAddBox>
+        </Icon>
+      </React.Fragment>
+    );
+  }
+  if (checkIcon) {
+    return (
+      <React.Fragment>
+        <Icon {...styles}>
+          <AiOutlineCheck size={size} onClick={_onClick}></AiOutlineCheck>
+        </Icon>
+      </React.Fragment>
+    );
+  }
+  if (leftArrowIcon) {
+    return (
+      <React.Fragment>
+        <Icon {...styles}>
+          <BiArrowBack size={size} onClick={_onClick}></BiArrowBack>
+        </Icon>
+      </React.Fragment>
+    );
+  }
 };
 
 // ImageButton DefaultProps
@@ -63,24 +93,18 @@ ImageButton.defaultProps = {
   size: "16px",
   height: "16px",
   margin: null,
-  _onChange: () => {},
-  onSubmit: () => {},
+  padding: null,
   _onClick: () => {},
 };
 
 // ImageButton 스타일드 컴포넌트
 const Icon = styled.div`
   margin: ${(props) => props.margin};
-  display: inline-block;
   cursor: pointer;
-  transition: all 0.2s;
+  width: ${(props) => props.width};
   height: ${(props) => props.height};
   margin: ${(props) => props.margin};
-  // hover 이벤트
-  &:hover {
-    color: #d85147;
-    transition: all 0.2s;
-  }
+  padding: ${(props) => props.padding};
 `;
 
 export default ImageButton;
