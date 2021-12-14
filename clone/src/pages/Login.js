91fbed34bf5107a8638d630eee416985f4f38a56
kongom2 <kongom2@jsceno.com>
2021-12-14 12:34:22 +0900

    로그인 회원가입

--- clone/src/pages/Login.js
+++ clone/src/pages/Login.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+
+// import { useDispatch } from "react-redux";
 
 // 엘레먼트 불러오기
 import Input from "../elements/Input";
@@ -7,9 +9,30 @@ import Image from "../elements/Image";
 import Button from "../elements/Button";
 
 const Login = () => {
+  //   const dispatch = useDispatch();
+
+  // 상태관리
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+
+  const login = () => {
+    // 이메일, 패스워드 빈값일 때 입력안내 문구
+    if (email === "") {
+      alert("이메일를 입력해주세요.");
+      return;
+    }
+    if (pwd === "") {
+      alert("비밀번호를 입력해주세요.");
+      return;
+    }
+  };
+
+  // 토큰 세션 확인
+  const is_session = localStorage.getItem("token");
+
   return (
     <React.Fragment>
-      <Grid>
+      <Grid width="350px" border="1px solid #e4e4e4" margin="100px auto">
         <Image
           width="190px"
           height="70px"
@@ -19,19 +42,37 @@ const Login = () => {
         />
         <Grid width="268px" margin=" 0 auto">
           <Input
+            _onChange={(e) => {
+              setEmail(e.target.value);
+            }}
             onSubmit={() => {}}
             placeholder="이메일 주소"
             padding="11px 0px 9px 8px"
             margin="5px auto"
           />
           <Input
+            _onChange={(e) => {
+              setPwd(e.target.value);
+            }}
             onSubmit={() => {}}
             placeholder="비밀번호"
             padding="11px 0px 9px 8px"
             margin="5px auto"
           />
-          <Button _onClick={() => {}} padding="8px 0px" margin="10px auto">
+          <Button
+            _onClick={login}
+            padding="8px 0px"
+            margin="10px auto 5px auto"
+          >
             로그인
+          </Button>
+          <Button
+            _onClick={login}
+            padding="8px 0px"
+            margin="5px auto 100px auto"
+            bold
+          >
+            가입하기
           </Button>
         </Grid>
       </Grid>
--- clone/src/pages/Signup.js
+++ clone/src/pages/Signup.js
@@ -1,48 +1,136 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
 // 엘레먼트 불러오기
 import Input from "../elements/Input";
 import Grid from "../elements/Grid";
 import Image from "../elements/Image";
 import Button from "../elements/Button";
+import Text from "../elements/Text";
+
+import { emailCheck, checkPassword } from "../shared/signupCheck";
 
 const Signup = () => {
+  //   const dispatch = useDispatch();
+
+  // 상태관리
+  const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [pwdCheck, setPwdCheck] = useState("");
+
+  // 토큰 세션 확인
+  const is_session = localStorage.getItem("token");
+
+  const signUp = () => {
+    if (email === "" || pwd === "" || userName === "") {
+      alert("입력사항을 전부 기입해주세요");
+      return;
+    }
+    if (!emailCheck(email)) {
+      alert("유효하지 않은 아이디 형식입니다.");
+      return;
+    }
+    if (!checkPassword(pwd)) {
+      alert("아이디가 포함되지 숫자+영문자 형식입니다");
+      return;
+    }
+    if (pwd.includes(email)) {
+      alert("비밀번호에 아이디가 포함되어 있습니다.");
+      return;
+    }
+    if (pwd !== pwdCheck) {
+      alert("비밀번호가 다릅니다.");
+      return;
+    }
+
+    // dispatch(userActions.signupDB(id, userName, pwd, pwdCheck));
+  };
+
   return (
     <React.Fragment>
-      <Grid>
+      <Grid width="350px" border="1px solid #e4e4e4" margin="100px auto">
         <Image
           width="190px"
           height="70px"
           backgroundSize="cover"
-          margin="90px auto 22px auto"
+          margin="22px auto 22px auto"
           imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2560px-Instagram_logo.svg.png"
         />
         <Grid width="268px" margin=" 0 auto">
           <Input
+            _onChange={(e) => {
+              setEmail(e.target.value);
+            }}
             onSubmit={() => {}}
             placeholder="이메일 주소"
             padding="11px 0px 9px 8px"
             margin="5px auto"
           />
+          <Grid hide={email === "" ? "none" : null}>
+            <Text
+              color={emailCheck(email) ? "#1fc40f" : "#ff5d5d"}
+              bold
+              margin="0px 0px 13px 5px"
+              size="0.75rem"
+            >
+              {emailCheck(email)
+                ? "올바른 양식의 아이디입니다"
+                : "올바르지 않는 아이디 형식입니다."}
+            </Text>
+          </Grid>
           <Input
+            _onChange={(e) => {
+              setUserName(e.target.value);
+            }}
             onSubmit={() => {}}
             placeholder="사용자 이름"
             padding="11px 0px 9px 8px"
             margin="5px auto"
           />
+
           <Input
+            _onChange={(e) => {
+              setPwd(e.target.value);
+            }}
             onSubmit={() => {}}
             placeholder="비밀번호"
             padding="11px 0px 9px 8px"
             margin="5px auto"
+            type="password"
           />
+          <Grid hide={pwd === "" ? "none" : null}>
+            <Text
+              color={
+                checkPassword(pwd) && pwd.includes(email) === false
+                  ? "#1fc40f"
+                  : "#ff5d5d"
+              }
+              bold
+              margin="0px 0px 13px 5px"
+              size="0.75rem"
+            >
+              {checkPassword(pwd) && pwd.includes(email) === false
+                ? "사용할 수 있는 비밀번호입니다"
+                : "아이디가 포함되지 않은 숫자+영문자 형식입니다."}
+            </Text>
+          </Grid>
           <Input
+            _onChange={(e) => {
+              setPwdCheck(e.target.value);
+            }}
             onSubmit={() => {}}
             placeholder="비밀번호 확인"
             padding="11px 0px 9px 8px"
             margin="5px auto"
+            type="password"
           />
-          <Button _onClick={() => {}} padding="8px 0px" margin="10px auto">
+
+          <Button
+            _onClick={signUp}
+            padding="8px 0px"
+            margin="10px auto 50px auto"
+          >
             가입
           </Button>
         </Grid>
