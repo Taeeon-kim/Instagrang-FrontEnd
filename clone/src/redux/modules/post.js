612a302032626be33414b72b7b066baf721c64de
kongom2 <kongom2@jsceno.com>
2021-12-15 21:30:15 +0900

    AddPost 작성 기능 추가

--- clone/src/redux/modules/post.js
+++ clone/src/redux/modules/post.js
@@ -4,26 +4,26 @@ import axios from "axios";
 import instance from "../../axios";
 import moment from "moment";
 
-
-const SET_POST = 'SET_POST'
-const ADD_POST = 'ADD_POST'
-const DELETE_POST = 'DELETE_POST'
-const GET_DETAIL ='GET_DETAIL'
-const IS_LIKE = 'IS_LIKE'
+const SET_POST = "SET_POST";
+const ADD_POST = "ADD_POST";
+const DELETE_POST = "DELETE_POST";
+const GET_DETAIL = "GET_DETAIL";
+const IS_LIKE = "IS_LIKE";
 // 액션 생성 함수
-const setPost = createAction(SET_POST,(post_list) => ({post_list}))
-const addPost = createAction(ADD_POST,(post) => ({post}))
-const deletePost = createAction(DELETE_POST ,(post) => ({post}))
-const getDetail = createAction(GET_DETAIL ,((post)=>({post})))
-const like = createAction(IS_LIKE, (like)=>({like}))
+const setPost = createAction(SET_POST, (post_list) => ({ post_list }));
+const addPost = createAction(ADD_POST, (image, content) => ({
+  image,
+  content,
+}));
+const deletePost = createAction(DELETE_POST, (post) => ({ post }));
+const getDetail = createAction(GET_DETAIL, (post) => ({ post }));
+const like = createAction(IS_LIKE, (like) => ({ like }));
 // 기본값 지정
 const initialState = {
-    
-    list:[] ,
-    detail: false,
-    is_like: false,
-}
-
+  list: [],
+  detail: false,
+  is_like: false,
+};
 
 const initalPost = {
   content: "",
@@ -36,14 +36,42 @@ const initalPost = {
 };
 
 // [공성훈] addPost 작업 중
-const addPostDB = () => {
-  //   console.log(selectedImage);
+const addPostDB = (image, content) => {
+  console.log(image);
+  //formData에 이미지 담기
   const formData = new FormData();
-  formData.append("image");
+  formData.append("image", image);
+  formData.append("content", content);
+  const TOKEN = localStorage.getItem("token");
+  console.log(TOKEN);
+  //   const options = {
+  //     headers: {
+  //       "content-type": "multipart/form-data",
+  //       authorization: `${TOKEN}`,
+  //     },
+  //     data: formData,
+  //   };
+
   return function (dispatch, getState, { history }) {
-    instance.post().then((res) => {
-      console.log("addPostDB 접근 확인");
-    });
+    // console.log(options.data);
+    // for (let value of options.data.values()) {
+    //   // formdata 확인, 콘솔로그로 확인 불가
+    //   console.log(value);
+    // }
+    axios
+      .post("http://3.36.100.253/api/posts", formData, {
+        headers: {
+          authorization: `${TOKEN}`,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        console.log("addPostDB 접근 확인");
+        dispatch(addPost());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 };
 
@@ -99,57 +127,68 @@ const getDetailPost = () => {
   };
 };
 
-
-const likePost =(postId) =>{
-    return function (dispatch, getState,{history}){
-        const TOKEN = localStorage.getItem("token");
-        console.log(postId);
-        axios.post(`http://3.36.100.253/api/likes/${postId}`,{} ,{ headers: {
-            "authorization" : `${TOKEN}`
-          }}).then((response)=>{
+const likePost = (postId) => {
+  return function (dispatch, getState, { history }) {
+    const TOKEN = localStorage.getItem("token");
+    console.log(postId);
+    axios
+      .post(
+        `http://3.36.100.253/api/likes/${postId}`,
+        {},
+        {
+          headers: {
+            authorization: `${TOKEN}`,
+          },
+        }
+      )
+      .then((response) => {
         console.log("like 눌름");
         console.log(response);
-        dispatch(like())
-    })
-}
-}
+        dispatch(like());
+      });
+  };
+};
 
 // 리듀서
 export default handleActions(
+  {
+    [SET_POST]: (state, action) =>
+      produce(state, (draft) => {
+        draft.list = action.payload.post_list;
+      }), // 리스트를 초기값에서 갈아끼우기
+    [ADD_POST]: (state, action) =>
+      produce(state, (draft) => {
+        draft.list.unshift(action.payload.post);
+        // draft.list = action.payload.post;
+        console.log(draft.list);
+      }),
+    [DELETE_POST]: (state, action) =>
+      produce(state, (draft) => {
+        // draft.study.joinNum -= 1;
+        // draft.join = action.payload.join
+      }),
 
-    {
-        [SET_POST] : (state,action) => produce(state,(draft) => {
-            draft.list = action.payload.post_list 
-        }),// 리스트를 초기값에서 갈아끼우기
-        [ADD_POST] : (state,action) => produce(state,(draft) => {
-                draft.list = action.payload.post
-                console.log(draft.list)
-        }),
-        [DELETE_POST] : (state, action) => produce(state, (draft) => {
-            // draft.study.joinNum -= 1;
-            // draft.join = action.payload.join
-        }),
-
-        [GET_DETAIL] : (state,action) => produce(state, (draft)=>{
-            draft.detail = true;
-        }),
-        [IS_LIKE] : (state, action) => produce(state, (draft) => {
-            draft.is_like ? draft.is_like = false : draft.is_like =true;
-        }),
-    }
-,initialState)
+    [GET_DETAIL]: (state, action) =>
+      produce(state, (draft) => {
+        draft.detail = true;
+      }),
+    [IS_LIKE]: (state, action) =>
+      produce(state, (draft) => {
+        draft.is_like ? (draft.is_like = false) : (draft.is_like = true);
+      }),
+  },
+  initialState
+);
 
 // 액션생성자 내보냄
 const actionCreators = {
-    setPost,
-    getMainAPI,
-    addPostDB, // [공성훈] 작업 중
-    addPost,
-    getDetailPost,
-    likePost,
-    like,
-    
-}
+  setPost,
+  getMainAPI,
+  addPostDB, // [공성훈] 작업 중
+  addPost,
+  getDetailPost,
+  likePost,
+  like,
+};
 
-export {actionCreators}
-
+export { actionCreators };
